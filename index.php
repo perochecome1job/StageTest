0f9858bddb7479e071373e1a437d666184f64d84
peroche <peroche@come1job.com>
2015-02-09 10:30:05 +0100

    test

---
+++ index.php
@@ -0,0 +1,5 @@
+Date : <?php echo 'd/m/Y'; 
+/*$link = mysql_connect("localhost", "root", "")
+    or die("Impossible de se connecter : " . mysql_error());
+echo 'Connexion réussie';
+mysql_close($link);*/
